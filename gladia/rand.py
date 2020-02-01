170da0982dd4e31e10b457d44664a4e19e784146
Johan Nestaas <johannestaas@gmail.com>
2020-02-01 14:54:31 -0800

    make it so it shuffles and iterates through them all

--- gladia/rand.py
+++ gladia/rand.py
@@ -1,9 +1,10 @@
 import os
-from random import choice, randint
+from random import shuffle, randint
 
 DIRNAME = os.path.dirname(__file__)
 with open(os.path.join(DIRNAME, 'data', 'names.txt')) as f:
     NAMES = [x.strip().title() for x in f.readlines() if x.strip()]
+    shuffle(NAMES)
 
 
 def roll(n, sides, top=None):
@@ -13,5 +14,17 @@ def roll(n, sides, top=None):
     return sum(rolls)
 
 
-def random_name():
-    return choice(NAMES).title()
+def _name_randomizer():
+    name_idx = 0
+
+    def random_name():
+        nonlocal name_idx
+        name = NAMES[name_idx].title()
+        name_idx += 1
+        name_idx %= len(NAMES)
+        return name
+
+    return random_name
+
+
+random_name = _name_randomizer()
