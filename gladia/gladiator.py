cfab302c5e949306546529bc8266cd43fbe25a19
Johan Nestaas <johannestaas@gmail.com>
2020-02-01 17:00:43 -0800

    mess with rand

--- gladia/gladiator.py
+++ gladia/gladiator.py
@@ -1,8 +1,12 @@
+import math
 import logging
 from dataclasses import dataclass
 
 from .rand import roll, random_name, attempt_with_bonus
 from .gear import Weapon, Armor
+
+CRITICAL_CHANCE = 0.05
+CRITICAL_COEF = 1.5
 
 LOG = logging.getLogger(__name__)
 
@@ -10,11 +14,15 @@ LOG = logging.getLogger(__name__)
 @dataclass
 class AttackResult:
     hit: bool
+    crit: bool
     dmg: int
 
     def __str__(self):
         if self.hit:
-            return f'Hit for {self.dmg} damage'
+            if self.crit:
+                return f'Critical Hit for {self.dmg} damage!'
+            else:
+                return f'Hit for {self.dmg} damage'
         else:
             return f'Miss'
 
@@ -24,9 +32,10 @@ class Gladiator:
     name: str
     hp: int
     max_hp: int
-    _attack: int
-    _agility: int
-    _intel: int
+    _atk: int
+    _def: int
+    _agi: int
+    _int: int
     weapon: Weapon
     armor: Armor
 
@@ -34,9 +43,10 @@ class Gladiator:
         self.name = random_name()
         self.hp = 100
         self.max_hp = 100
-        self._attack = roll(4, 6, top=3)
-        self._agility = roll(4, 6, top=3)
-        self._intel = roll(4, 6, top=3)
+        self._atk = roll(4, 6, top=3)
+        self._def = roll(4, 6, top=3)
+        self._agi = roll(4, 6, top=3)
+        self._int = roll(4, 6, top=3)
         self.weapon = Weapon.fist()
         self.armor = Armor.skin()
 
@@ -44,32 +54,42 @@ class Gladiator:
         for i, msg in enumerate([
             f'Gladiator {self.name}',
             f'HP:       {self.hp}/{self.max_hp}',
-            f'Atk:      {self._attack}',
-            f'Agi:      {self._agility}',
-            f'Int:      {self._intel}',
+            f'Atk:      {self._atk}',
+            f'Def:      {self._def}',
+            f'Agi:      {self._agi}',
+            f'Int:      {self._int}',
             f'Wpn:      {self.weapon!r}',
             f'Arm:      {self.armor!r}',
         ]):
             win.write(msg, pos=(0, i))
 
     def hit_or_miss(self, enemy):
-        diff = self._agility - enemy._agility
+        diff = self._agi - enemy._agi
         return attempt_with_bonus(self.weapon.acc, bonus=diff)
+
+    def check_critical(self, enemy):
+        diff = self._atk - enemy._def
+        LOG.debug(f'checking critical of {self.name} vs {enemy.name}')
+        return attempt_with_bonus(CRITICAL_CHANCE, bonus=diff)
 
     def attack(self, enemy):
         LOG.debug(f'{self.name} attacking {enemy.name}')
         if not self.hit_or_miss(enemy):
             LOG.debug(f'{self.name} missed!')
-            return AttackResult(hit=False, dmg=0)
+            return AttackResult(hit=False, crit=False, dmg=0)
         LOG.debug(f'{self.name} hit!')
         dmg_result = self.weapon.damage()
         LOG.debug(f'result: {dmg_result!r}')
         dmg_result.subtract_armor(enemy.armor)
         LOG.debug(f'after subtracting enemy armor: {dmg_result!r}')
         dmg_type, dmg = dmg_result.max()
+        crit = self.check_critical(enemy)
+        if crit:
+            dmg = math.ceil(dmg * CRITICAL_COEF)
+            LOG.debug(f'attack is critical!')
         enemy.take_damage(dmg)
         LOG.debug(f'{self.name} did {dmg_type} damage for {dmg}')
-        return AttackResult(hit=True, dmg=dmg)
+        return AttackResult(hit=True, crit=crit, dmg=dmg)
 
     def take_damage(self, amt):
         self.hp -= amt
--- gladia/rand.py
+++ gladia/rand.py
@@ -1,6 +1,9 @@
 import os
 import math
+import logging
 from random import shuffle, randint, uniform
+
+LOG = logging.getLogger(__name__)
 
 DIRNAME = os.path.dirname(__file__)
 with open(os.path.join(DIRNAME, 'data', 'names.txt')) as f:
@@ -37,17 +40,20 @@ def rand_prob(mn=0, mx=1):
 
 def attempt_with_bonus(base, bonus=0):
     prob = rand_prob()
-    if bonus == 0:
+    LOG.debug(
+        f'attempt with bonus, base: {base}, bonus: {bonus}, prob: {prob}'
+    )
+    if bonus <= 0:
         # If no bonus, just check the roll against the base.
         # If accuracy (base) is 0.9, then we're checking a 90% rate of hit.
         return prob <= base
     # Use a logarithmic curve to determine the bonus.
     # The log value affects whether the difficulty is magnified or not.
     val = math.log(abs(bonus) + math.e)
-    if bonus > 0:
-        difficulty = (1.0 - base) / val
-    else:
-        difficulty = (1.0 - base) * val
+    LOG.debug(f'math.log(abs(bonus) + math.e) = {val}')
+    # This calc is waaay too helpful.
+    difficulty = (1.0 - base) / val
+    LOG.debug(f'difficulty is now {1.0 - difficulty}')
     return prob <= (1.0 - difficulty)
 
 
